12ba53cbfc53b34cbaf96b54c8f7c0394dfdf932
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-09-23 20:07:56 +0200

    Refactoring

--- src/Trackr/app.js
+++ src/Trackr/app.js
@@ -1,25 +1,42 @@
 Ext.Loader.setConfig({
-	disableCaching: false
-});
+		disableCaching: false
+	});
 
 Ext.require('Ext.direct.Manager');
 
-Ext.onReady(function () {
+Ext.onReady(function() {
 	Ext.direct.Manager.addProvider(Trackr.server.REMOTING_API);
-	Ext.direct.Manager.addListener('exception', function (e) {
+	Ext.direct.Manager.addListener('exception', function(e) {
 		Ext.Error.raise(Ext.String.format('{0}.{1} - {2}', e.action, e.method, e.message));
 	}, this);
 
 	Ext.create('Ext.app.Application', {
 		name: 'Trackr',
-		autoCreateViewport: true,
+
 		controllers: [
 			'TaskList',
 			'TaskEdit',
 			'Login'
 		],
 
-		launch: function () {
+		launch: function() {
+			Ext.widget('viewport', {
+				layout: 'fit',
+				items: [{
+					xtype: 'panel',
+					layout: 'card',
+					items: [{
+						xtype: 'taskeditpanel',
+						itemId: 'taskedit',
+						border: 0
+					}, {
+						xtype: 'tasklistpanel',
+						itemId: 'tasklist',
+						border: 0,
+						padding: 10
+					}]
+				}]
+			});
 		}
 	});
 });

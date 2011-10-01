9bfd4ac17c3a5e548c824af176e420fb7537b1a4
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-01 19:51:45 +0200

    Added automatic build

---
+++ build.cmd
@@ -0,0 +1,2 @@
+@echo off
+tools\jsmake\jsmake.cmd %*
---
+++ build.js
@@ -0,0 +1,25 @@
+load('tools/jslint/jslint.js');
+var uglifyjs = require('tools/uglifyjs/uglify-js');
+
+var fs = jsmake.Fs;
+var utils = jsmake.utils;
+
+task('default', 'buildjs');
+
+task('buildjs', function () {
+	var appFiles = jsmake.Fs.createScanner('src/Trackr/app')
+		.include('**/*.js')
+		.scan();
+	var appContent = jsmake.Utils.map(appFiles, function (appFile) {
+		return fs.readFile(appFile);
+	}).join('\n');
+
+	var mainFiles = jsmake.Fs.createScanner('src/Trackr')
+		.include('*-dev.js')
+		.scan();
+	jsmake.Utils.each(mainFiles, function (mainFile) {
+		var content = fs.readFile(mainFile);
+		content = [ appContent, content ].join('\n'); //uglifyjs([ appContent, content ].join('\n'));
+		fs.writeFile(mainFile.replace('-dev.js', '.js'), content);
+	});
+});

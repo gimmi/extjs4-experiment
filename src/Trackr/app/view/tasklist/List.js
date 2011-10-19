1be47244d3d27927baedb438ba0d5b67ea9c28ad
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-19 14:51:34 +0200

    Configured checkbox selection model on grid

--- src/Trackr/app/view/tasklist/List.js
+++ src/Trackr/app/view/tasklist/List.js
@@ -3,15 +3,20 @@ Ext.define('Trackr.view.tasklist.List', {
 	alias: 'widget.tasklistlist',
 	
 	requires: [ 'Trackr.store.TaskInfos' ],
+	
+	columns: [
+		{ text: 'Title', dataIndex: 'title' },
+		{ text: 'State', dataIndex: 'state' },
+		{ text: 'Description', dataIndex: 'description', flex: 1 }
+	],
+	
+	selModel: {
+		selType: 'checkboxmodel',
+		mode: 'MULTI'
+	},
 
 	initComponent: function () {
 		this.store = Ext.create('Trackr.store.TaskInfos');
-
-		this.columns = [
-			{ text: 'Title', dataIndex: 'title' },
-			{ text: 'State', dataIndex: 'state' },
-			{ text: 'Description', dataIndex: 'description', flex: 1 }
-		];
 
 		this.dockedItems = [{
 			dock: 'bottom',
